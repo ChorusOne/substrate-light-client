d12c1e981289b71e970fb898f523e41740735652
Parth Desai <desaiparth08@gmail.com>
2020-04-01 15:14:54 +0530

    call grandpa module func

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ mod babe;
 use sp_runtime::traits::{Block as BlockT};
 
 fn ingest_finalized_header<B: BlockT>(finalized_header: &B::Header) -> Result<(), &str> {
+    let _scheduled_authority_change = grandpa::find_scheduled_change::<B>(finalized_header);
     Ok(())
 }
 
