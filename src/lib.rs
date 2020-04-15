85a74b9d7ddaa956e60210917a31a4bbad69fab2
Parth Desai <desaiparth08@gmail.com>
2020-04-15 23:57:05 +0530

    add serializable memory db

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
-mod grandpa;
-mod babe;
+mod db;
 
 use sp_runtime::traits::{Block as BlockT};
 
+
 fn ingest_finalized_header<B: BlockT>(finalized_header: &B::Header) -> Result<(), &str> {
-    let _scheduled_authority_change = grandpa::find_scheduled_change::<B>(finalized_header);
     Ok(())
 }
 
