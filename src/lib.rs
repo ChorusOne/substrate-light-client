93762c0fd65d0aab306a09d000bfe7dd7952edd8
Parth Desai <desaiparth08@gmail.com>
2020-04-16 00:00:02 +0530

    remove dummy test

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,7 @@ mod db;
 
 use sp_runtime::traits::{Block as BlockT};
 
-
+// WASM entry point
 fn ingest_finalized_header<B: BlockT>(finalized_header: &B::Header) -> Result<(), &str> {
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
 }
