8b0ddaa3f5bf56c3e45eb4cb9eceed17e76bdc88
Parth Desai <desaiparth08@gmail.com>
2020-05-28 10:48:30 +0530

    deleting files from root directory

--- src/genesis.rs
+++
@@ -1,4 +0,0 @@
-use parity_scale_codec::{Decode, Encode};
-
-#[derive(Encode, Decode, Clone)]
-pub struct GenesisData {}
--- src/types.rs
+++
@@ -1,5 +0,0 @@
-use sp_runtime::traits::BlakeTwo256;
-use sp_runtime::OpaqueExtrinsic;
-
-pub type Header = sp_runtime::generic::Header<u32, BlakeTwo256>;
-pub type Block = sp_runtime::generic::Block<Header, OpaqueExtrinsic>;
