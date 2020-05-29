884582d16ddb6295e440ac4db449e5e23d634fd2
Parth Desai <desaiparth08@gmail.com>
2020-05-29 11:48:17 +0530

    adding best hash

--- substrate_client/src/common.rs
+++ substrate_client/src/common.rs
@@ -83,6 +83,7 @@ where
     pub possible_finalized_header: Option<Block::Header>,
     pub possible_light_authority_set: Option<LightAuthoritySet>,
     pub possible_next_change_in_authority: Option<NextChangeInAuthority<Block>>,
+    pub possible_best_header: Option<Block::Header>,
 }
 
 pub fn initialize_backend(
