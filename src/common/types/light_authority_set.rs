25eaba3195c1b4e4398ba96c0ba008a02fdd6e90
Parth Desai <desaiparth08@gmail.com>
2020-06-18 13:52:34 +0530

    remove unwanted trait impl

--- src/common/types/light_authority_set.rs
+++ src/common/types/light_authority_set.rs
@@ -1,9 +1,8 @@
 use parity_scale_codec::{Decode, Encode};
-use serde::{Deserialize, Serialize};
 use sp_finality_grandpa::AuthorityList;
 
 /// Latest authority set tracker.
-#[derive(Debug, Encode, Decode, Clone, Serialize, Deserialize, Default)]
+#[derive(Debug, Encode, Decode, Clone, Default)]
 pub struct LightAuthoritySet {
     set_id: u64,
     authorities: AuthorityList,
